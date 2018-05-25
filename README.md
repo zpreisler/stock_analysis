57000a91589a7a697c99e40090f4ef78188b4d14
Zdenek Preisler <z.preisler@gmail.com>
2018-05-24 17:29:08 -0700

    Added plot

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#Stock analysis
+# Stock analysis
--- alpha_vantage.py
+++ alpha_vantage.py
@@ -12,6 +12,9 @@ class data:
         self.open,self.high,self.low,self.close,self.volume=self.get()
 
     def get(self):
+        """
+        Convert json data into arrays
+        """
         from numpy import array
         key=sorted(list(list(self.data.values())[0].keys()))
         v=[[] for _ in range(len(key))]
@@ -19,6 +22,38 @@ class data:
             for i,k in enumerate(key):
                 v[i]+=[float(self.data[t][k])]
         return array(v)
+
+    def plot(self):
+        """
+        Plot data
+        """
+        from matplotlib.pyplot import show,figure,subplot
+        import matplotlib.gridspec as gridspec
+        from numpy import array,arange
+        gs=gridspec.GridSpec(2,1,width_ratios=[1], height_ratios=[3,1])
+
+        fig=figure(figsize=(10,6))
+        ax1=subplot(gs[0])
+        ax2=subplot(gs[1],sharex=ax1)
+
+        h=array(self.close-self.open) #bar heigth
+        x=arange(len(h))
+
+        p=h[h>0]; m=h[h<0]
+        xp=x[h>0]; xm=x[h<0]
+        bp=self.open[h>0]; bm=self.open[h<0]
+
+        ax1.set_title(self.json['Meta Data']['2. Symbol'])
+        ax1.vlines(x,ymin=self.low,ymax=self.high,linewidth=0.66)
+        ax1.bar(xp,p,bottom=bp,color='g',width=0.66)
+        ax1.bar(xm,m,bottom=bm,color='r',width=0.66)
+        ax1.set_ylabel(r"price [\,\$\,]")
+
+        ax2.bar(x,self.volume,alpha=0.33)
+        ax2.set_ylabel(r"volume")
+        ax2.set_xlabel(r"date")
+
+        fig.subplots_adjust(hspace=0,left=0.1)
 
 class vantage:
     """
@@ -51,23 +86,15 @@ class vantage:
 
 def main():
 
-    a=vantage()
+    stock=vantage()
 
-    a.request({'symbol':'GOOG'})
-    a.request({'symbol':'AMZN'})
-    print(a.GOOG.open)
-    from matplotlib.pyplot import show,figure,plot
-
-    figure()
-    plot(a.GOOG.low,'k-',alpha=0.1,linewidth=0.5)
-    plot(a.GOOG.high,'k-',alpha=0.1,linewidth=0.5)
-
-    plot(a.AMZN.low,'k-',alpha=0.1,linewidth=0.5)
-    plot(a.AMZN.high,'k-',alpha=0.1,linewidth=0.5)
-
-    figure()
-    plot(a.GOOG.volume,'k-',alpha=0.1,linewidth=0.5)
-
+    stock.request({'symbol':'GOOG'})
+    #stock.request({'symbol':'AMZN'})
+    #stock.request({'symbol':'IBM'})
+    from matplotlib.pyplot import show
+    stock.GOOG.plot()
+    #stock.AMZN.plot()
+    #stock.IBM.plot()
     show()
 
 if __name__=="__main__":
@@ -75,6 +102,3 @@ if __name__=="__main__":
         main()
     except KeyboardInterrupt:
         print("Ctrl-C")
-
-
-
